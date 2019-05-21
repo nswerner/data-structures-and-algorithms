e1b349d67ead7756be25b1a39400440104306524
Nicholas Werner <nswerner@ucdavis.edu>
2019-05-21 16:34:49 -0400

    Solve LeetCode Coin-Change-2, #516

--- memoization_project/lib/leet_code_518.js
+++ memoization_project/lib/leet_code_518.js
@@ -1,3 +1,18 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+
+var change = function (amount, coins, memo = {}) {
+  let key = amount + '-' + coins;
+  if (key in memo) return memo[key];
+  if (amount === 0) return 1;
+
+  let currentCoin = coins[coins.length - 1];
+  let total = 0;
+  for (let qty = 0; qty * currentCoin <= amount; qty++) {
+    total += change(amount - qty * currentCoin, coins.slice(0, - 1), memo);
+  }
+
+  memo[key] = total;
+  return memo[key];
+};
