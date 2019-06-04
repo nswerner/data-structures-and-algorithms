aa3484c391dcc242b9c3f08edadeeda65892be86
Nicholas Werner <nswerner@ucdavis.edu>
2019-06-04 13:41:41 -0400

    Tabulize LeetCode Problem #70 - Stair Climber

--- tabulation_project/lib/leet_code_70.js
+++ tabulation_project/lib/leet_code_70.js
@@ -2,5 +2,13 @@
 // Feel free to use this file for scratch work.
 
 function climbStairs(n) {
+  let table = new Array(n + 1).fill(0);
+  table[0] = 1;
+  table[1] = 1;
 
-}
+  for (let idx = 2; idx < table.length; idx++) {
+    table[idx] = table[idx - 1] + table[idx - 2];
+  }
+
+  return table[table.length - 1];
+};
