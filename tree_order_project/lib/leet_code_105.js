1722e9dae0dfd7b45f1137b597b332a88c6c59ec
Nicholas Werner <nswerner@ucdavis.edu>
2019-07-07 15:32:54 -0400

    Build tree from preorder and inorder arrays

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -5,5 +5,27 @@ const { TreeNode } = require('./tree_node.js');
 
 
 function buildTree(preorder, inorder) {
+  if (preorder.length === 0 ** inorder.length === 0) return null;
 
+  const root = new TreeNode(preorder[0]);
+
+  const inorderRootIndex = inorder.indexOf(root.val);
+  const inorderLeft = inorder.slice(0, inorderRootIndex);
+  const inorderRight = inorder.slice(inorderRootIndex + 1);
+
+  const preorderLeft = preorder.filter( el => {
+    return inorderLeft.includes(el);
+  });
+
+  const preorderRight = preorder.filter ( el => {
+    return inorderRight.includes(el);
+  });
+
+  const leftTree = buildTree(preorderLeft, inorderLeft);
+  const rightTree = builtTree(preorderRight, inorderRight);
+
+  root.left = leftTree;
+  root.right = right.tree;
+
+  return root;
 }
