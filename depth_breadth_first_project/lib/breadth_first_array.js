617436c331f932ec708dc3faa249219df2ff71e6
Nicholas Werner <nswerner@ucdavis.edu>
2019-07-07 22:44:18 -0400

    Code Iterative Breadth First Array

--- depth_breadth_first_project/lib/breadth_first_array.js
+++ depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,5 +1,21 @@
 function breadthFirstArray(root) {
+    const queue = [ root ];
+    const result = [];
+    
+    while (queue.length) {
+        let node = queue.shift();
+        result.push(node.val);
 
+        if (node.left) {
+            queue.push(node.left);
+        }
+
+        if (node.right) {
+            queue.push(node.right);
+        }
+    }
+
+    return result;
 }
 
 module.exports = {
