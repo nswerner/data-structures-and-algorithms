4746300bb2870e1a29c221274305a309369b5366
Nicholas Werner <nswerner@ucdavis.edu>
2019-07-08 00:06:26 -0400

    Code Tree Height and Tree Sum

--- depth_breadth_first_project/lib/tree_height.js
+++ depth_breadth_first_project/lib/tree_height.js
@@ -1,5 +1,10 @@
 function treeHeight(root) {
+    if (!root) return -1;
 
+    const leftHeight = treeHeight(root.left) + 1;
+    const rightHeight = treeHeight(root.right) + 1;
+    
+    return Math.max(leftHeight, rightHeight);
 }
 
 
--- depth_breadth_first_project/lib/tree_sum.js
+++ depth_breadth_first_project/lib/tree_sum.js
@@ -1,5 +1,21 @@
 function treeSum(root) {
+    const queue = !!root ? [root] : [];
+    let result = 0;
 
+    while (queue.length) {
+        let node = queue.shift();
+        result += node.val;
+
+        if (node.left) {
+            queue.push(node.left);
+        }
+
+        if (node.right) {
+            queue.push(node.right);
+        }
+    }
+
+    return result;
 }
 
 
