f0491937f0f746e6c366f3523a020cbcd081ea79
Nicholas Werner <nswerner@ucdavis.edu>
2019-07-11 15:51:22 -0400

    Code Tree - Is Balanced?

--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -2,6 +2,21 @@
 //  https://leetcode.com/problems/balanced-binary-tree/
 
 
-function isBalanced(root) {
+var getHeight = function (root) {
+  if (!root) return -1;
 
+  return 1 + Math.max(getHeight(root.left), getHeight(root.right));
 }
+
+var isBalanced = function (root) {
+  if (!root) return true;
+
+  if (Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1) {
+    const leftBalance = isBalanced(root.left);
+    const rightBalance = isBalanced(root.right);
+
+    return leftBalance && rightBalance;
+  } else {
+    return false;
+  };
+};
