6b8155442df5814ed701d274a0e5797d87ad524d
Nicholas Werner <nswerner@ucdavis.edu>
2019-07-11 16:08:08 -0400

    Convert Sorted Array into BST

--- bst_project/lib/leet_code_108.js
+++ bst_project/lib/leet_code_108.js
@@ -2,6 +2,20 @@
 //  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
 
-function sortedArrayToBST(nums) {
+var sortedArrayToBST = function (nums) {
+  if (nums.length <= 0) return null;
 
-}
+  const midIdx = Math.floor(nums.length / 2);
+  const midEle = new TreeNode(nums[midIdx]);
+
+  const left = nums.slice(0, midIdx);
+  const right = nums.slice(midIdx + 1);
+
+  const leftTree = sortedArrayToBST(left);
+  const rightTree = sortedArrayToBST(right);
+
+  midEle.left = leftTree;
+  midEle.right = rightTree;
+
+  return midEle;
+};
