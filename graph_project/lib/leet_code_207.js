35fb86bec6e1c4926f03a3cdb9a9badafb74dd15
Nicholas Werner <nswerner@ucdavis.edu>
2019-07-24 02:55:28 -0400

    Code LeetCode#207 - Course Schedule

--- graph_project/lib/leet_code_207.js
+++ graph_project/lib/leet_code_207.js
@@ -1,6 +1,46 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/course-schedule/
 
-function canFinish(numCourses, prerequisites) {
+var createGraph = function (prereqs) {
+  const graph = {};
 
+  for (let idx = 0; idx < prereqs.length; idx++) {
+    [course, prereq] = prereqs[idx].map(String);
+
+    if (course in graph) {
+      graph[course].push(prereq);
+    } else {
+      graph[course] = [prereq];
+    }
+
+    if (!(prereq in graph)) {
+      graph[prereq] = [];
+    }
+  }
+
+
+  return graph;
 }
+
+
+var canFinish = function (numCourses, prerequisites) {
+  const prereqs = createGraph(prerequisites);
+  const completed = new Set();
+  let validCourse = true;
+
+  while (validCourse) {
+    validCourse = false;
+
+    for (let course in prereqs) {
+      if (!completed.has(course) && prereqs[course].every(preq => completed.has(preq))) {
+        completed.add(course);
+        validCourse = true;
+      }
+    }
+  }
+
+  return completed.size === Object.keys(prereqs).length;
+}
+
+
+console.log(canFinish(2, [[1,0]]));
