5ebc9b290cbbfbbad30a420efe06e667dba6ce5a
Nicholas Werner <nswerner@ucdavis.edu>
2019-08-27 13:08:13 -0400

    UniqueChar

--- cracking_code_interview/arrays_strings/uniqueChar.js
+++ cracking_code_interview/arrays_strings/uniqueChar.js
@@ -1,8 +1,46 @@
 // Implement an algorithm to determine if a string has all unique characters. 
 // What if you cannot use additional data structures?
 
-const hasUniqueChar = function(string) {
-  
+// Listen, Example, Brute, Optimize, Walk Through, Implement, Test
+// BUD (bottlenecks, unnecessary work, duplicated work), DIY, Simplify and Generalize, Base Case, DS, BCR
+
+// Brute => 0(n^2) - inner loop returns false if second occurence of char is found, return true @ the end
+// Optimize => set.has(char) ? return false : set.add(char)
+// No additional data structures => sacrifice time for space => sort and look for repeats
+
+let hasUniqueChar = function(string) {
+  const charSet = new Set();
+
+  for (let i = 0; i < string.length; i++) {
+    if (charSet.has(string[i])) return false;
+    charSet.add(string[i]);
+  }
+
+  return true;
 }
+
+console.log(hasUniqueChar("hello")) //  => false
+console.log(hasUniqueChar("James")) // => true
+console.log(hasUniqueChar("Jj")) // => true
+
+
+hasUniqueChar = function(string) {
+  const sortedString = string.split('').sort();
+  
+  for (let i = 0; i < sortedString.length; i++) {
+    const char = sortedString[i];
+    const next = sortedString[i+1];
+
+    if (char === next) return false;
+  }
+
+  return true;
+}
+
+console.log(hasUniqueChar("hello")) //  => false
+console.log(hasUniqueChar("James")) // => true
+console.log(hasUniqueChar("Jj")) // => true
+
+
 
 
